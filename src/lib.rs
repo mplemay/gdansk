3ddf983f550cfea5ec1cbf78115c668451e0024a
Matt LeMay <mplemay@users.noreply.github.com>
2026-02-15 13:29:29 -0500

    feat(rust): added an async demo function

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 use pyo3::prelude::*;
 
-/// A Python module implemented in Rust. The name of this module must match
-/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
-/// import the module.
 #[pymodule]
 mod _core {
     use pyo3::prelude::*;
@@ -10,5 +7,13 @@ mod _core {
     #[pyfunction]
     fn hello_from_bin() -> String {
         "Hello from gdansk!".to_string()
+    }
+
+    #[pyfunction]
+    fn rust_sleep(py: Python) -> PyResult<Bound<PyAny>> {
+        pyo3_async_runtimes::tokio::future_into_py(py, async {
+            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+            Ok(())
+        })
     }
 }
