06577b9b7fc20489eb9015c699862d7db25bd3af
Matt LeMay <mplemay@users.noreply.github.com>
2026-04-07 22:57:45 -0500

    Remove root Rust crate and switch to uv_build

--- src/plugins/vite_fs.js
+++
@@ -1,8 +0,0 @@
-import promises from "node:fs/promises";
-
-function realpathSync(path) {
-  return Deno.core.ops.op_gdansk_vite_realpath_sync(path);
-}
-
-export { promises, realpathSync };
-export default { promises, realpathSync };
