455ceb68bcace3f4841e27c2749bad2a98fa954b
Matt LeMay <mplemay@users.noreply.github.com>
2026-04-08 22:38:47 -0500

    refactor: removed old python files

--- src/gdansk/__init__.py
+++
@@ -1,4 +0,0 @@
-from gdansk.core import Page, Ship
-from gdansk.plugins import LightningCSS, VitePlugin, ViteScript
-
-__all__ = ["LightningCSS", "Page", "Ship", "VitePlugin", "ViteScript"]
--- src/gdansk/__tests__/fixtures/views/widgets/with_css/widget.tsx
+++
@@ -1,5 +0,0 @@
-import "./simple.css";
-
-export default function App() {
-    return null;
-}
